1937c4bbfd874aee54e74c3bf1e59bc788e67a34
Touhed <mr.touhed.i@outlook.com>
2024-10-06 16:30:04 +0600

    commet reffer section admin

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -7,7 +7,7 @@ const Dashboardlayout = ({children}) => {
         <ValidedUser>
         <div className="grid grid-cols-[320px_1fr]">
             <SideMenu/>
-            <section className="min-h-[100vh]">
+            <section className="min-h-[100vh] mt-16">
                     {children}
             </section>
         </div>
