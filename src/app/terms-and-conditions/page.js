c4bbd28e7b134fbab837a1cd2cfdef29ee377c57
Touhed <mr.touhed.i@outlook.com>
2024-10-28 23:07:12 +0600

    deploy for test

--- src/app/terms-and-conditions/page.js
+++ src/app/terms-and-conditions/page.js
@@ -9,12 +9,12 @@ const TermsAndConditionPage = () => {
             </HeadingAnimation>
 
             <section>
-                    <h2 className='md:text-3xl text-lg font-semibold  '> 1. Introduction:</h2>
+                    <h2 className='text-base  font-semibold  '> 1. Introduction:</h2>
                     <p className='ml-2 text-justify'>Youman (“we,” “us,” or “our”) values your privacy and is committed to protecting your personal information. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our services, including the Youman NFC card. Please read this policy carefully to understand our practices regarding your data.
                     </p>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg  font-semibold  '> 2. Information We Collect:</h2>
+            <h2 className='text-base  font-semibold  '> 2. Information We Collect:</h2>
                     <p className='ml-2'>We may collect and process the following data:</p>
                     <ul className='list-inside list-disc ml-4'>
                         <li>Personal Information: Such as your name, email address, phone number, and address when you sign up for our services.</li>
@@ -25,7 +25,7 @@ const TermsAndConditionPage = () => {
                     </ul>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 3. How We Use Your Information:</h2>
+            <h2 className='text-base font-semibold  '> 3. How We Use Your Information:</h2>
                     <p className='ml-2'>We use your data for the following purposes:</p>
                     <ul className='list-inside list-disc ml-4'>
                         <li>Service Delivery: To provide you with access to our platform, membership benefits, and community interactions.</li>
@@ -38,7 +38,7 @@ const TermsAndConditionPage = () => {
                     </ul>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 4. Sharing Your Information:</h2>
+            <h2 className='text-base font-semibold  '> 4. Sharing Your Information:</h2>
             <p className='ml-2'>We may share your information in the following scenarios:
             </p>
             <ul className='list-inside list-disc ml-4'>
@@ -50,19 +50,19 @@ const TermsAndConditionPage = () => {
             </ul>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 5. Data Security:
+            <h2 className='text-base font-semibold  '> 5. Data Security:
             </h2>
             <p className='ml-2'>We implement industry-standard security measures to protect your personal information. However, no method of transmission over the internet or electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your data, we cannot guarantee its absolute security.
             </p>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '>6. Data Retention:
+            <h2 className='text-base font-semibold  '>6. Data Retention:
             </h2>
             <p className='ml-2'>We retain your personal information only for as long as necessary to fulfill the purposes outlined in this policy, comply with legal obligations, resolve disputes, and enforce our agreements. Data may be anonymized and retained for analytics beyond this period.</p>
             </section>
 
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 7. Your Rights:
+            <h2 className='text-base font-semibold  '> 7. Your Rights:
             </h2>
             <p className='ml-2'>You have the following rights concerning your personal information:
 
@@ -80,18 +80,18 @@ const TermsAndConditionPage = () => {
             </ul>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 8. Cookies and Tracking Technologies:</h2>
+            <h2 className='text-base font-semibold  '> 8. Cookies and Tracking Technologies:</h2>
             <p className='ml-2'>We use cookies and similar technologies to track user activity on our website, improve user experience, and analyze website performance. You can control your cookie settings through your browser.
             </p>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 9. Changes to This Privacy Policy:</h2>
+            <h2 className='text-base font-semibold  '> 9. Changes to This Privacy Policy:</h2>
             <p className='ml-2 '>We reserve the right to modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised policy on our website. We encourage you to review this policy periodically to stay informed about how we are protecting your information.
 
             </p>
             </section>
             <section>
-            <h2 className='md:text-3xl text-lg font-semibold  '> 10. Contact Us:
+            <h2 className='text-base font-semibold  '> 10. Contact Us:
             </h2>
             <p className='ml-2'>If you have any questions about this Privacy Policy, please contact us at: <span className='underline text-green-700'>hello@youman.com.bd</span> or <span className='underline text-green-700'> hello@youman.one</span> 
 
